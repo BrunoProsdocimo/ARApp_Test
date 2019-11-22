2ef109a57e2ac86649ec1bdd61fc781a6f986e7e
BrunoProsdocimo <47759353+BrunoProsdocimo@users.noreply.github.com>
2019-11-22 15:58:35 -0300

    Unity setup and app setup
    Setup the project in unity with all necessary packages and started the app development. The app is able to recognize lit surfaces and place an indicator for objects to be placed.

---
+++ ARApp_Test/Assets/Zenva/Scripts/PlacementIndicator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    public GameObject visual;
+    public Trackable curTrackable;
+
+    private void Start()
+    {
+        visual.SetActive(false);
+    }
+
+    private void Update()
+    {
+        TrackableHit hit;
+        TrackableHitFlags filter = TrackableHitFlags.PlaneWithinPolygon;
+
+        if(Frame.Raycast(Screen.width / 2, Screen.height / 2, filter, out hit))
+        {
+            curTrackable = hit.Trackable;
+
+            transform.position = hit.Pose.position;
+            transform.rotation = hit.Pose.rotation;
+
+            visual.SetActive(true);
+        }
+        else
+        {
+            curTrackable = null;
+            visual.SetActive(false);
+        }
+    }
+}
