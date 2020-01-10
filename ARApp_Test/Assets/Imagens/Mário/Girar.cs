9eaa338619774aaa82851caf34f5852c36807222
BrunoProsdocimo <brunocprosdocimo@gmail.com>
2020-01-09 22:48:13 -0300

    Primeira tentativa de girar a cabeça
    Alguns arquivos aleatórios deletados e a primeira tentativa de girar a cabeça. Ainda está para ser testado

---
+++ ARApp_Test/Assets/Imagens/Mário/Girar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Girar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GirarCabeca());
+    }
+
+    IEnumerator GirarCabeca()
+    {
+        transform.Rotate(0, 0, 90);
+
+        yield return new WaitForSeconds(0.5f);
+    }
+}
