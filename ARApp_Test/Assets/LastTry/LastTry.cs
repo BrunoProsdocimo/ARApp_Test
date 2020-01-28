2288a2506649e6c4aa3d1328a9fba8d61e134c60
BrunoProsdocimo <brunocprosdocimo@gmail.com>
2020-01-28 19:01:25 -0300

    Última tentativa de interação
    Última tentativa de interação com os objetos em Realidade Aumentada via Raycast pela Câmera.

---
+++ ARApp_Test/Assets/LastTry/LastTry.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class LastTry : MonoBehaviour
+{
+    public Camera cam;
+    VideoScript[] videosInScene;
+    Vector2 touchPosition = default;
+    int index;
+
+
+    private void Start()
+    {
+        cam = Camera.main;
+        videosInScene = FindObjectsOfType<VideoScript>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            touchPosition = touch.position;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = cam.ScreenPointToRay(touch.position);
+                RaycastHit hitObj;
+                if (Physics.Raycast(ray, out hitObj))
+                {
+                    var selection = hitObj.transform.gameObject;
+                    if (selection.CompareTag("VideoPlayer"))
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+
+    void Selection(VideoScript selectedVideo)
+    {
+        foreach (VideoScript videosArray in videosInScene)
+        {
+            selectedVideo = videosArray;
+            if (videosArray.isSelected == false)
+            {
+                videosArray.isSelected = true;
+                videosArray.showMarker();
+            }
+            else
+            {
+                videosArray.isSelected = false;
+                videosArray.showMarker();
+            }
+        }
+    }
+}
---
+++ ARApp_Test/Assets/LastTry/VideoScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoScript : MonoBehaviour
+{
+    public bool isSelected;
+    public GameObject checkMarker;
+
+    public void showMarker()
+    {
+        if (isSelected == true)
+        {
+            checkMarker.SetActive(true);
+        }
+        else
+        {
+            checkMarker.SetActive(false);
+        }
+    }
+}
